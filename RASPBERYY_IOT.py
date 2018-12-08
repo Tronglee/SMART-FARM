a4ebbf462d095e1db77fe34182e1873122d4010a
Tronglee <thanhtrong1605@gmail.com>
2018-12-08 16:22:18 +0700

    Update RASPBERYY_IOT.py

--- RASPBERYY_IOT.py
+++ RASPBERYY_IOT.py
@@ -17,18 +17,18 @@ ADAFRUIT_IO_KEY = 'e2cac4fce1de4ebb802250a704153fa0'
 # (go to https://accounts.adafruit.com to find your username)
 ADAFRUIT_IO_USERNAME = 'trongle'
 # Set to the ID of the feed to subscribe to for updates.
-Relay_1 = 'relay1'///pump out
-Relay_2 = 'relay2'///fan
-Relay_3 = 'relay3'///heating lamp
-Relay_4 = 'relay4'///pump in
-Relay_5 = 'relay5'///led light
+Relay_1 = 'relay1' #pump out
+Relay_2 = 'relay2' #fan
+Relay_3 = 'relay3' #heating lamp
+Relay_4 = 'relay4' #pump in
+Relay_5 = 'relay5' #led light
 Threhold = 'threhold'
 Settime = 'settime'
 
 print("IoT Farm Project - Graduation Project")
 print("Adafruit MQTT connected - >>...Start...<< ")
 def connected(client):
-    # Subscribe to changes on a feed named Counter.
+    						# Subscribe to changes on a feed named Counter.
     print('Subscribing to Feed {0}'.format(Relay_1))
     client.subscribe(Relay_1)
 
@@ -53,7 +53,7 @@ def connected(client):
 def disconnected(client):
     """Disconnected function will be called when the client disconnects."""
     sys.exit(1)
-
+					##message
 def message(client, feed_id, payload):
     """Message function will be called when a subscribed feed has a new value.
     The feed_id parameter identifies the feed, and the payload parameter has
@@ -61,19 +61,19 @@ def message(client, feed_id, payload):
     """
     print('Feed {0} received new value: {1}'.format(feed_id, payload))
 
-    #Threhold detect value
+  							  #Threhold detect value
     if(feed_id == 'threhold'):
         print('Temperature Threhold value: ', payload)
         print('Set Temperature threshold to Device\n')
         ser.write(('threhold'+ payload + '\r').encode())      
         sleep(1)
-
+ 							#settime detect value
     if(feed_id == 'settime'):
         print('Timer Threhold value: ', payload)
         print('Set Timer to Device\n')
         ser.write(('timerSet'+ payload + '\r').encode())      
             
-    #Relay 1
+    #Relay 1				##relay on, off
     if(feed_id == 'relay1'):
         if(payload == '1'):
             print('Turn on Relay 1\n')
@@ -135,13 +135,13 @@ def message(client, feed_id, payload):
     
     
 # Create an MQTT client instance.
-client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)		##???
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)			##???
 
 # Create an MQTT client instance.
 client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-##try: # if we have a 'type' feed
+						##try: # if we have a 'type' feed
 Temperature = aio.feeds('temperature')
 Humidity = aio.feeds('humidity')
 Relay1 = aio.feeds('relay1')
@@ -152,38 +152,13 @@ Relay5 = aio.feeds('relay5')
 threhold = aio.feeds('threhold')
 settime = aio.feeds('settime')
 Foodremain = aio.feeds('foodremain')
-##except RequestError: # create a type feed
-##    #Sensor Feeds 
-##    feed = Feed(name="temperature")
-##    Temperature = aio.create_feed(feed)
-##
-##    feed = Feed(name="humidity")
-##    Humidity = aio.create_feed(feed)
-##
-##    feed = Feed(name="gas")
-##    Gas = aio.create_feed(feed)
-##
-##    feed = Feed(name="fire")
-##    Fire = aio.create_feed(feed)
-##    
-##    #Relay Feeds 
-##    feed = Feed(name="relay1")
-##    Relay1 = aio.create_feed(feed)
-##
-##    feed = Feed(name="relay2")
-##    Relay2 = aio.create_feed(feed)
-##
-##    feed = Feed(name="relay3")
-##    Relay3 = aio.create_feed(feed)
-##
-##    feed = Feed(name="relay4")
-##    Relay4 = aio.create_feed(feed)
+
 
 
 loop_delay = 5    
 ser = serial.Serial(
 	port = '/dev/ttyAMA0',
-	baudrate = 115200,
+	baudrate = 115200,				##115200
 	parity = serial.PARITY_NONE,
 	stopbits = serial.STOPBITS_ONE,
 	bytesize = serial.EIGHTBITS,
@@ -191,27 +166,8 @@ ser = serial.Serial(
 )
 
 
-    
-##def findFromStringData(s):
-##    for item in indata:
-##        if(item.find('temp')) != -1:
-##            temp = item[5:]
-##            print('Temperature is: ' +temp)
-##
-##        if(item.find('humi')) != -1:
-##            humid = item[5:]
-##            print('Humidity is: ' +humid)
-##
-##        if(item.find('gas ')) != -1:
-##            gas = item[5:]
-##            print('Gas is: ' +gas)
-##
-##        if(item.find('fire')) != -1:
-##            fire = item[5:]
-##            print('Fire is: ' +fire)
-##        
 def relaySwitch():
-            #Relay 1
+            #Relay 1					## nhan dlieu
         relay1Data = aio.receive(Relay1.key)
         relay2Data = aio.receive(Relay2.key)
         relay3Data = aio.receive(Relay3.key)
@@ -269,9 +225,9 @@ client.on_connect = connected
 client.on_disconnect = disconnected
 client.on_message = message
 
-# Connect to the Adafruit IO server.
+						# Connect to the Adafruit IO server.
 client.connect()
-client.loop_background()
+client.loop_background()			##lap
 
 
 try:
@@ -291,7 +247,7 @@ try:
                 indata.append(data)
             if len(indata) == 5:
                 print(indata)
-                print('sending data to adafruit io...')
+                print('sending data to adafruit io...') 		##sending data to adafruit io...
                 for item in indata:
                     
                     if(item.find('temp')) != -1:
@@ -318,7 +274,7 @@ try:
                     if(item.find('relay1')) != -1:
                         relay1 = item[7:]
                         print('Relay 1 update value: ', relay1)
-                        aio.send(Relay1.key, relay1)
+                        aio.send(Relay1.key, relay1)		##update
 
                     if(item.find('relay2')) != -1:
                         relay2 = item[7:]
